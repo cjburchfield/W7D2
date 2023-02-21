9752b5edaa5124653f847b540537685e9a8a05dc
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-02-21 14:33:47 -0500

    Warm up phase work

---
+++ Jamie_Music_App/jamie_music_app/app/models/user.rb
@@ -0,0 +1,57 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        bcypt_obj =  BCrypt::Password.new(self.password_digest)
+        bcypt_obj.is_password?(password)
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+end
